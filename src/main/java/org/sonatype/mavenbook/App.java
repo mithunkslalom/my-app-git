d3996d295d892284a2d8d3b5b7bf9fedf2d65b0a
Mithun Kumar <mithun.kumar@univarusa.com>
2016-09-30 03:00:43 -0700

    fourth commit

--- src/main/java/org/sonatype/mavenbook/App.java
+++ src/main/java/org/sonatype/mavenbook/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello all , welcome to BAI!" );
+        System.out.println( "Hello all , welcome to BAI , happy friday!" );
     }
 }
